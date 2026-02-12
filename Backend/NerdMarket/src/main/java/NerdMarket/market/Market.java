b6594d6ab4ce8debe6eb7a3a951b62a11bad776e
Hrushi <hrushibhatt@gmail.com>
2026-02-12 17:14:45 -0600

    Created market entity with JPA annotations + table auto created using hibernate

--- Backend/NerdMarket/src/main/java/NerdMarket/market/Market.java
+++ Backend/NerdMarket/src/main/java/NerdMarket/market/Market.java
@@ -1,7 +1,30 @@
 //This file will map to the Market table in MySQL.
-//Will define fields such as Title, Category, Card Value, etc..
+//Will define fields such as Title, Category, Card Value, etc...
 
 package NerdMarket.market;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Market {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String cardType;
+    private String cardName;
+    private String cardSet;
+    private String cardRarity;
+    private double price;
+
 }

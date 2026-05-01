3af13e94dad83fdc755a4bbe6ff823fda984c9e0
Bhatt, Hrushi <hbhatt10@iastate.edu>
2026-05-01 10:57:32 -0500

    add HrushiSystemTest file for system tests

---
+++ Backend/src/test/java/NerdMarket/HrushiSystemTest.java
@@ -0,0 +1,23 @@
+package NerdMarket;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class HrushiSystemTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+        RestAssured.baseURI = "http://localhost";
+    }
+
+}
